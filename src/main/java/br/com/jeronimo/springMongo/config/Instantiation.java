4037cb2cde20c48c5bfdc579326fb6b6b33510e4
Jeronimo Hermano <jeronimohermano.nc@gmail.com>
2020-01-31 11:42:11 -0300

    Criando postagens com usuário aninhado

--- src/main/java/br/com/jeronimo/springMongo/config/Instantiation.java
+++ src/main/java/br/com/jeronimo/springMongo/config/Instantiation.java
@@ -1,27 +1,43 @@
 package br.com.jeronimo.springMongo.config;
 
+import br.com.jeronimo.springMongo.domain.Post;
 import br.com.jeronimo.springMongo.domain.User;
+import br.com.jeronimo.springMongo.repository.PostRepository;
 import br.com.jeronimo.springMongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
   @Autowired
   private UserRepository userRepository;
+  @Autowired
+  private PostRepository postReposiroty;
 
   @Override
-  public void run(String... args) {
+  public void run(String... args) throws ParseException {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
     User u1 = new User(null, "Maria Brown", "maria@gmail.com");
     User u2 = new User(null, "Alex Gray", "alex@gmail.com");
     User u3 = new User(null, "Bob Blue", "bob@gmail.com");
 
     userRepository.deleteAll();
     userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
+    Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
+    Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", u1);
+
+    postReposiroty.saveAll(Arrays.asList(post1, post2));
+
   }
 
 }
---
+++ src/main/java/br/com/jeronimo/springMongo/domain/Comment.java
@@ -0,0 +1,9 @@
+package br.com.jeronimo.springMongo.domain;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document
+public class Comment implements Serializable {
+}
